85048281db9dede15dc61e713f198b4ac15106e8
dormantroot <dormantroot@gmail.com>
2011-12-20 06:58:06 -0500

    Signed-off-by: dormantroot <dormantroot@gmail.com> Finished the required code to implement CREATE service

--- REST_in_WCF/APIs/OrderApi.cs
+++ REST_in_WCF/APIs/OrderApi.cs
@@ -13,19 +13,31 @@ namespace REST_in_WCF.APIs
     [ServiceContract]
     public class OrderApi
     {
-         [WebInvoke(UriTemplate = "", Method = "POST")]
-        public HttpResponseMessage<Order> Post(Order customerOrder)
-         {
-             if (!String.IsNullOrEmpty(customerOrder.Status))
-             {
-                return new HttpResponseMessage<Order>(HttpStatusCode.BadRequest);
-             }
-             else
-             {
-                 customerOrder.Status = "pending";
-                 return new HttpResponseMessage<Order>(customerOrder, HttpStatusCode.Created);
-             }
-             
-         }  
+        [WebInvoke(UriTemplate = "", Method = "POST")]
+        public HttpResponseMessage<Order> Post(HttpRequestMessage<Order> request, Order customerOrder)
+        {
+            HttpResponseMessage<Order> response = null;
+
+            try
+            {
+                if (!String.IsNullOrEmpty(customerOrder.Status))
+                {
+                    response = new HttpResponseMessage<Order>(HttpStatusCode.BadRequest);
+                }
+                else
+                {
+                    customerOrder.Status = "pending";
+                    response = new HttpResponseMessage<Order>(customerOrder, HttpStatusCode.Created);
+                    response.Headers.Location = new Uri(request.RequestUri, "order/12345");
+                }
+
+                return response;
+            }
+            catch (Exception)
+            {
+                return response = new HttpResponseMessage<Order>(HttpStatusCode.InternalServerError);
+            }
+
+        }  
     }
 }
