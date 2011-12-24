85bf24fa888c1c0c11e06865efb5a3e26b92ecaa
dormantroot <dormantroot@gmail.com>
2011-12-24 07:48:02 -0500

    Signed-off-by: dormantroot <dormantroot@gmail.com> Completed code for Rest in WCF Part 5

--- REST_in_WCF/APIs/OrderApi.cs
+++ REST_in_WCF/APIs/OrderApi.cs
@@ -67,7 +67,50 @@ namespace REST_in_WCF.APIs
             {
                 return response = new HttpResponseMessage<Order>(HttpStatusCode.InternalServerError);
             }
-        }  
+        }
+
+        [WebInvoke(UriTemplate = "{id}", Method = "PUT")]
+        public HttpResponseMessage<Order> UpdateOrder(int id, Order customerOrder)
+        {
+            HttpResponseMessage<Order> response = null;
+
+            try
+            {
+                if (!String.IsNullOrEmpty(customerOrder.Status))
+                {
+                    response = new HttpResponseMessage<Order>(HttpStatusCode.BadRequest);
+                }
+                else
+                {
+                    // For brevity, I'm assuming that order - 12345 was already served, while 123456 wasn't.
+                    // Note: The code doesn't actual update an existing order b/c I'm not persisting anything
+                    // to a datastore.
+                    // Just for the sake of demonstration, I'm modifying the incoming Order object.
+                    if (id == 12345)
+                    {
+                        customerOrder.Status = "already served";
+                        return new HttpResponseMessage<Order>(customerOrder, HttpStatusCode.Conflict);
+                    }
+
+                    if (id == 123456)
+                    {
+                        customerOrder.Items.FirstOrDefault().Milk = "skim";
+                        customerOrder.Items.FirstOrDefault().Size = "large";
+                        customerOrder.Status = "preparing";
+                        return new HttpResponseMessage<Order>(customerOrder, HttpStatusCode.OK);
+                    }
+
+                    // return '404 - Not Found' status code
+                    response = new HttpResponseMessage<Order>(HttpStatusCode.NotFound);
+                }
+
+                return response;
+            }
+            catch (Exception)
+            {
+                return response = new HttpResponseMessage<Order>(HttpStatusCode.InternalServerError);
+            }
+        }
 
     }
 }
